439e88345b08d2d1976dc27ebda3d93046fcb0a9
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-12-16 17:24:48 +0200

    Create state validator middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,6 +1,5 @@
 export default ({ dispatch }) => next => action => {
   // Check to see if the action has a promise
-  debugger;
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
